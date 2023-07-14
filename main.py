b22d9f69b088401c78e2d0c3539fd1eb67ea5493
MyGamesM <milanmilan.mm55@gmail.com>
2023-07-14 17:47:27 +0200

    Test commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from classes.map import Map
+from time import sleep
+
+os.system('clear')
+
+class App():
+    def __init__(self) -> None:
+        self.map = Map(4, 4)
+
+        self.input = int(input("1: Single\n2: Loop\n$ "))
+
+        if self.input == 1:
+            os.system('clear')
+            self.map.generateMap()
+            self.map.print_map()
+            
+        elif self.input == 2:
+            self.time = float(input("Loop delay: "))
+            while True:
+                os.system('clear')
+                self.map.generateMap()
+                self.map.print_map()
+                sleep(self.time)
+
+if __name__ == "__main__":
+    app = App()
